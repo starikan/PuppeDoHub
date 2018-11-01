9a9828d42ed650806c1ceaa01b4585f4b25a6b73
Sergey Starodubov <4analit@gmail.com>
2018-11-01 16:59:12 +0400

    Init test instance

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const _ = require('lodash');
+const CircularJSON = require('circular-json');
 const puppedo = require('PuppeDo');
+var path = require('path');
+var express = require('express');
 
-let tests = {};
+let sockets = {};
 
 let eventsOn = {
   // create_envs: async (data) => {
@@ -16,100 +19,134 @@ let eventsOn = {
   // },
   init_test: async ({
     payload,
-    envsId,
     socket
   }) => {
     try {
       let response = {};
       try {
-        let envs = {};
-        if (!envsId) {
+        if (!_.get(socket, 'envsId')) {
+          socket.ppd = puppedo.env();
+          await socket.ppd.envs.init(payload, false);
+          socket.envsId = _.get(socket.ppd, 'envsId');
+          socket.envs = _.get(socket.ppd, 'envs');
+          socket.log = _.get(socket.ppd, 'log');
           console.log('init_test', payload);
-          let ppd = puppedo.env();
-          await ppd.envs.init(payload, false);
-          console.log('createEnvs', ppd);
-          envsId = _.get(ppd, 'envsId');
-          envs = _.get(ppd, 'envs');
-          tests[envsId] = ppd;
-        } else {
-          envs = _.get(tests, [envsId, 'envs'], {});
+          console.log('createEnvs', socket);
         }
-        response.message = 'init_test';
-        response.envsId = envsId;
-        response.payload = {
-          envs: _.isObject(envs) ? _.omit(envs, ['init', 'log']) : {},
+        response = {
+          message: 'init_test',
+          payload: {
+            envsId: socket.envsId,
+            envs: socket.envs
+          }
         };
-      }
-      catch (error){
+      } catch (error) {
         console.log(error);
-        response.message = 'error';
-        response.error = error;
-        response.stack = _.get(error, 'stack');
+        response = {
+          message: 'error',
+          payload: {
+            error: error,
+            stack: _.get(error, 'stack'),
+          }
+        }
+      } finally {
+        socket.send(CircularJSON.stringify(response));
       }
-
-      socket.send(JSON.stringify(response));
-
     } catch (err) {
       console.log(err)
     }
   },
-  // get_json: (data) => {
-  //   try {
-  //     console.log('get_json', this);
-  //     let envs = _.get(this, "ppd.envs");
-  //     const fullJSON = getFullDepthJSON({
-  //       envs: envs,
-  //       filePath: envs.get('args.testFile'),
-  //     });
-  //     console.log(fullJSON)
-  //     this.emit('news', fullJSON);
-  //   }
-  //   catch (err) {
-  //     console.log(err)
-  //   }
-  // }
+  get_json: ({
+    payload,
+    socket
+  }) => {
+
+    // try {
+    //   let response = {};
+    //   try {
+    //     let envs = ;
+    //     if (!envsId) {
+    //       console.log('init_test', payload);
+    //       let ppd = puppedo.env();
+    //       await ppd.envs.init(payload, false);
+    //       console.log('createEnvs', ppd);
+    //       envsId = _.get(ppd, 'envsId');
+    //       envs = _.get(ppd, 'envs');
+    //       tests[envsId] = ppd;
+    //     } else {
+    //       envs = _.get(tests, [envsId, 'envs'], {});
+    //     }
+    //     response.message = 'init_test';
+    //     response.envsId = envsId;
+    //     response.payload = {
+    //       envs: _.isObject(envs) ? _.omit(envs, ['init', 'log']) : {},
+    //     };
+    //   } catch (error) {
+    //     console.log(error);
+    //     response.message = 'error';
+    //     response.error = error;
+    //     response.stack = _.get(error, 'stack');
+    //   }
+
+    //   socket.send(JSON.stringify(response));
+
+    // } catch (err) {
+    //   console.log(err)
+    // }
+
+
+    // try {
+    //   console.log('get_json', this);
+    //   let envs = _.get(this, "ppd.envs");
+    //   const fullJSON = getFullDepthJSON({
+    //     envs: envs,
+    //     filePath: envs.get('args.testFile'),
+    //   });
+    //   console.log(fullJSON)
+    //   this.emit('news', fullJSON);
+    // } catch (err) {
+    //   console.log(err)
+    // }
+  }
 }
 
 function runWsServer() {
-  var path = require('path');
-  var express = require('express');
-  var app = express();
-  var ws = require('express-ws')(app);
+  const app = express();
+  let ws = require('express-ws')(app);
 
   app.use(express.static(__dirname + '/static'));
 
   app.get('/', (req, res) => {
-    console.error('express connection');
     res.sendFile(path.join(__dirname, 'index.html'));
   });
 
   app.ws('/', (socket, req) => {
     console.log('websocket connection');
-    socket.on('message', async data => {
-      console.log(socket)
-      const incomeData = JSON.parse(data);
-      const message = _.get(incomeData, 'message');
-      const envsId = _.get(incomeData, 'envsId');
+
+    // console.log(app.listeners())
+
+    socket.onmessage = async event => {
+      const incomeData = JSON.parse(event.data);
       const payload = _.get(incomeData, 'payload');
-      const funcOn = _.get(eventsOn, message);
-      if (message && funcOn) {
+      const funcOn = _.get(eventsOn, _.get(incomeData, 'message'));
+      if (funcOn) {
         await funcOn({
           payload,
-          envsId,
           socket
         });
-        // debugger
       }
-    });
-    socket.on('close', () => {});
+    };
+
+    socket.onclose = () => {};
+    socket.onerror = () => {};
+    socket.onopen = () => {};
   });
 
-  app.listen(3001, () => console.error('listening on http://localhost:3001/'));
-  console.error('websocket example');
+  app.listen(3001, () => console.log('listening on http://localhost:3001/'));
 }
 
 if (!module.parent) {
   runWsServer();
 } else {
-  exports.runWsServer = runWsServer;
+  module.exports = { runWsServer };
 }
--- static/sockets.js
+++ static/sockets.js
@@ -1,20 +1,12 @@
-let envsId;
-
 let socket = new WebSocket('ws://' + window.location.host + '/');
-socket.addEventListener('error', function (m) {
-  console.log(m, "error");
-});
-socket.addEventListener('open', function (m) {
-  console.log(m, "websocket connection open");
-});
-socket.onmessage = function (event) {
+
+socket.onerror = m => console.log(m, "error");
+
+socket.onopen = m => console.log(m, "websocket connection open");
+
+socket.onmessage = event => {
   let data = JSON.parse(event.data);
-  let message = _.get(data, 'message');
-  let payload = _.get(data, 'payload');
-  let envsId = _.get(data, 'envsId');
-
   console.log(data)
-
 };
 
 let init_test = () => {
@@ -22,9 +14,10 @@ let init_test = () => {
   socket.send(JSON.stringify({
     message: 'init_test',
     payload: {
-      envsId,
+      // envsId,
       test: 'testGlob',
-      envs: ["settings/settings/envCloud.yaml"],
+      testsFolder: 'settings',
+      envs: ["settings/envCloud.yaml"],
     }
   }));
 }
