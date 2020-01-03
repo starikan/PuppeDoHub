1cdba414f5c3ddf93d21ce9367a4524105049557
Sergey Starodubov <4analit@gmail.com>
2020-01-03 11:55:48 +0400

    Refactoring

--- index.js
+++ index.js
@@ -1,71 +1,52 @@
-const state = {};
-let output, websocket;
+let websocket;
 
-const args = {
-  PPD_ROOT: 'tests',
-  PPD_ENVS: 'mainEnv',
-  PPD_TESTS: 'main',
-  PPD_DEBUG_MODE: 'true',
-};
-
+// INIT EVENTS LISTENERS
 function init() {
-  output = document.getElementById('output');
-
   document.getElementById('runServer').addEventListener('click', runServerClick);
   document.getElementById('argsInit').addEventListener('click', argsInitClick);
 }
-
-async function runServerClick(event) {
-  testWebSocket('ws://127.0.0.1:3001/');
-}
-
-async function argsInitClick(event) {
-  const data = JSON.stringify({ data: args, method: 'argsInit' });
-  doSend(data);
-}
-
-function testWebSocket(wsUri) {
-  websocket = new WebSocket(wsUri);
-  websocket.onopen = function(evt) {
-    onOpen(evt);
-  };
-  websocket.onclose = function(evt) {
-    onClose(evt);
-  };
-  websocket.onmessage = function(evt) {
-    onMessage(evt);
-  };
-  websocket.onerror = function(evt) {
-    onError(evt);
-  };
-}
-
-function onOpen(evt) {
-  writeToScreen('CONNECTED');
-}
-
-function onClose(evt) {
-  writeToScreen('DISCONNECTED');
-}
-
-function onMessage(evt) {
-  writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data + '</span>');
-}
-
-function onError(evt) {
-  writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
-}
-
-function doSend(message) {
-  writeToScreen('SENT: ' + message);
-  websocket.send(message);
-}
+window.addEventListener('load', init);
 
 function writeToScreen(message) {
   const pre = document.createElement('p');
   pre.style.wordWrap = 'break-word';
   pre.innerHTML = message;
+
+  const output = document.getElementById('output');
   output.appendChild(pre);
 }
 
-window.addEventListener('load', init, false);
+// EVENTS
+
+async function runServerClick(event) {
+  function testWebSocket(wsUri) {
+    websocket = new WebSocket(wsUri);
+    websocket.onopen = function(evt) {
+      writeToScreen('CONNECTED');
+    };
+    websocket.onclose = function(evt) {
+      writeToScreen('DISCONNECTED');
+    };
+    websocket.onmessage = function(evt) {
+      writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data + '</span>');
+    };
+    websocket.onerror = function(evt) {
+      writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+    };
+  }
+
+  testWebSocket('ws://127.0.0.1:3001/');
+}
+
+async function argsInitClick(event) {
+  const args = {
+    PPD_ROOT: 'tests',
+    PPD_ENVS: 'mainEnv',
+    PPD_TESTS: 'main',
+    PPD_DEBUG_MODE: 'true',
+  };
+
+  const data = JSON.stringify({ data: args, method: 'argsInit' });
+  writeToScreen('SENT: ' + data);
+  websocket.send(data);
+}
