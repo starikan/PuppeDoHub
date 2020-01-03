d3145c6bf092d3d568b81c660bd1a2ba94bb17a8
Sergey Starodubov <4analit@gmail.com>
2020-01-03 19:55:44 +0400

    createEnvs

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let websocket;
+let envsIdServer;
 
 // INIT EVENTS LISTENERS
 function init() {
   document.getElementById('runServer').addEventListener('click', runServerClick);
   document.getElementById('argsInit').addEventListener('click', argsInitClick);
   document.getElementById('getAllTestsData').addEventListener('click', getAllTestsDataClick);
+  document.getElementById('createEnvs').addEventListener('click', createEnvsClick);
 }
 window.addEventListener('load', init);
 
@@ -29,10 +31,17 @@ async function runServerClick(event) {
       writeToScreen('DISCONNECTED');
     };
     websocket.onmessage = function(evt) {
-      writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data + '</span>');
+      const { data, type, envsId } = jsyaml.load(evt.data);
+      envsIdServer = envsId;
+      console.log(data, type, envsId);
+      if (type === 'error') {
+        writeToScreen('<span style="color: red;">ERROR: \n' + evt.data + '</span>');
+      } else {
+        writeToScreen('<span style="color: blue;">RESPONSE: \n' + evt.data + '</span>');
+      }
     };
     websocket.onerror = function(evt) {
-      writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+      writeToScreen('<span style="color: red;">ERROR: \n' + evt.data + '</span>');
     };
   }
 
@@ -47,13 +56,19 @@ async function argsInitClick(event) {
     PPD_DEBUG_MODE: 'true',
   };
 
-  const data = JSON.stringify({ data: args, method: 'argsInit' });
+  const data = JSON.stringify({ data: args, method: 'argsInit', envsId: envsIdServer });
   writeToScreen('SENT: ' + data);
   websocket.send(data);
 }
 
 async function getAllTestsDataClick(event) {
-  const data = JSON.stringify({ method: 'getAllTestsData' });
+  const data = JSON.stringify({ method: 'getAllTestsData', envsId: envsIdServer });
+  writeToScreen('SENT: ' + data);
+  websocket.send(data);
+}
+
+async function createEnvsClick(event) {
+  const data = JSON.stringify({ method: 'createEnvs', envsId: envsIdServer });
   writeToScreen('SENT: ' + data);
   websocket.send(data);
 }
