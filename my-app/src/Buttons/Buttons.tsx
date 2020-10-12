4d2da33a8ac87074279036c6accc9b95d14fa4b9
Sergey Starodubov <4analit@gmail.com>
2020-10-12 18:49:01 +0400

    Buttons refuctoring

--- my-app/src/Buttons/Buttons.tsx
+++ my-app/src/Buttons/Buttons.tsx
@@ -1,41 +1,42 @@
-import React from 'react';
-
-type MyProps = {};
-type MyState = { buttons: TButton[] };
+import React, { useState } from 'react';
 
 type TButton = { id: string; name: string };
 
-export default class Buttons extends React.Component<MyProps, MyState> {
-  styles: React.CSSProperties = {
-    border: '2px solid red',
-  };
+function Buttons() {
+  const [buttons] = useState([
+    { id: 'runServer', name: 'Запуск сервера PuppeDo' },
+    { id: 'argsInit', name: 'Инициализация аргументов' },
+    { id: 'getAllTestsData', name: 'Получить все данные тестов' },
+    { id: 'createEnvs', name: 'Запустить Environments' },
+    { id: 'setCurrentTest', name: 'Установить текущий тест' },
+    { id: 'runCurrentTest', name: 'Запустить текущий тест' },
+  ] as TButton[]);
 
-  state: MyState = {
-    buttons: [
-      { id: 'runServer', name: 'runServer' },
-      { id: 'argsInit', name: 'argsInit' },
-      { id: 'getAllTestsData', name: 'getAllTestsData' },
-      { id: 'createEnvs', name: 'createEnvs' },
-      { id: 'setCurrentTest', name: 'setCurrentTest' },
-      { id: 'runCurrentTest', name: 'runCurrentTest' },
-    ],
-  };
-
-  clickButtonHandler = (v: TButton) => {
+  const clickButtonHandler = (v: TButton) => {
     console.log(v.id);
   };
 
-  render() {
-    return (
-      <div>
-        {this.state.buttons.map((v) => {
-          return (
-            <button id={v.id} onClick={this.clickButtonHandler.bind(null, v)}>
-              {v.name}
-            </button>
-          );
-        })}
-      </div>
-    );
-  }
+  const stylesContainer: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    width: 200,
+  };
+
+  const stylesButtons: React.CSSProperties = {
+    width: 200,
+  };
+
+  return (
+    <div style={stylesContainer}>
+      {buttons.map((v) => {
+        return (
+          <button id={v.id} key={v.id} onClick={clickButtonHandler.bind(null, v)} style={stylesButtons}>
+            {v.name}
+          </button>
+        );
+      })}
+    </div>
+  );
 }
+
+export default Buttons;
