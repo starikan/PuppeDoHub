e7c401fbc73c543a24585665736e379d29b6b7ee
Sergey Starodubov <4analit@gmail.com>
2021-02-01 08:16:29 +0400

    Functional component tree

--- my-app/src/Tree/Tree.tsx
+++ my-app/src/Tree/Tree.tsx
@@ -1,42 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import SortableTree, { TreeItem } from 'react-sortable-tree';
 import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
 import data from '../Data';
 
-const ppd2tree = (d: any): TreeItem[] => {
+const ppd2tree = (d: any): TreeItem => {
   const { name, runTest = [], description } = d;
   const children = runTest.map((v: any) => ppd2tree(v));
   return { ...d, ...{ title: description || name, subtitle: name, children } };
 };
 
-type StateType = { treeData: TreeItem[], nodeClicked?: any }
+const Tree = () => {
+  const [tree, setTree] = useState([ppd2tree(data)]);
+  const [nodeClicked, setNodeClicked] = useState(ppd2tree(data));
 
-export default class Tree extends Component<{}, StateType> {
-  constructor(props: any) {
-    super(props);
-    this.state = { treeData: [ppd2tree(data)] } ;
-  }
+  return (
+    <div style={{ height: 800 }}>
+      <SortableTree
+        treeData={tree}
+        onChange={(treeData: TreeItem[]) => setTree(treeData)}
+        generateNodeProps={(rowInfo) => {
+          const { node } = rowInfo;
+          // console.log(rowInfo);
+          return {
+            onClick: () => {
+              setNodeClicked(node);
+            },
+            style:
+              node === nodeClicked
+                ? {
+                    border: '3px solid yellow',
+                  }
+                : {},
+          };
+        }}
+      />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div style={{ height: 800 }}>
-        <SortableTree
-          treeData={this.state.treeData}
-          onChange={(treeData) => this.setState({ treeData })}
-          generateNodeProps={(rowInfo) => {
-            // const {node} = rowInfo;
-            // console.log(rowInfo);
-            return {
-              // onClick: () => {
-                // console.log(node)
-                // debugger;
-              // },
-              style: { border: '3px solid yellow' },
-            };
-          }}
-        />
-        {/* <span>{JSON.stringify(this.state.treeData, null, 2)}</span> */}
-      </div>
-    );
-  }
-}
+export default Tree;
